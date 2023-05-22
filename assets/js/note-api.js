837c8a03b8b6cd22e1f4762e43b23642775f465e
DucLong <duclong@fullstack.edu.vn>
2023-05-22 22:32:24 +0700

    Add logic javascript and API

---
+++ assets/js/note-api.js
@@ -0,0 +1,16 @@
+/* API
+- Method: 
+    - GET: lấy dữ liệu
+    - POST: gửi dữ liệu lên -> tạo dữ liệu mới
+    - PUT: gửi dữ liệu lên -> sửa dữ liệu
+    - PATCH: gửi dữ liệu -> sửa dữ liệu
+    - DELETE: xóa dữ liệu
+
+- PUT: Sửa lại toàn bộ các trường dữ liệu
+- PATCH: Sửa lại 1 số trường dữ liệu
+
+- status code: Là các bộ mã thể hiện trạng thái của api
+    - 2xx: Thành công
+    - 4xx: Lỗi do client
+    - 5xx: Lỗi server
+*/
